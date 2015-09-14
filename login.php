cb501f7a241dd07fd569654633a8c3f5159569eb
Mikhail Kasyanov <michaelrain@exsul.net>
2015-09-14 15:55:42 -0400

    Login ver 0.0.1

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php 
+session_start();
+
+
+include("includes/header.php"); 
+error_reporting(E_ALL);
+ini_set("display_errors", 1); //Ошибки 
+
+require 'vendor/autoload.php';
+include_once('vendor/enelar/phpsql/phpsql.php'); //Включения для подключения 
+include_once('vendor/enelar/phpsql/pgsql.php');
+include_once('vendor/enelar/phpsql/wrapper.php');
+$sql = new phpsql();
+$pg = $sql->Connect("pgsql://postgres@localhost/synchrotalk");
+include_once('vendor/enelar/phpsql/db.php');
+db::Bind(new phpsql\utils\wrapper($pg)); 
+
+
+if(isset($_SESSION["session_username"])){
+  header("Location:intropage.php");
+}
+
+if(isset($_POST["login"])){
+  if(!empty($_POST['username']) && !empty($_POST['password'])) {
+    $username=($_POST["username"]);
+    $password=($_POST["password"]);
+    $users_query=db::Query("SELECT * FROM users WHERE username=$1 AND password=$2",[$username,$password]); //Запрос
+    var_dump($users_query);
+    if($users_query())
+    {
+     // while($users_query) //Преврщаем его в ассоц массив
+     // {
+     //   $dbusername=$row['username'];
+     //   $dbpassword=$row['password'];
+     // }
+      exit();
+      if ($dbusername==$username && $dbpassword==$password){
+        $_SESSION['session_username']=$username;
+        header("Location: intropage.php");
+      }
+    }
+      else {
+      echo "Не правильный Логин или Пароль";} 
+
+    }
+    else {
+    echo "Поля не заполнены.";
+  }
+}
+
+?>
+
+
+
+<div class="container mlogin">
+<div id="login">
+<h1>Входs</h1>
+<form action="" id="loginform" method="post"name="loginform">
+<p><label for="user_login">Имя опльзователя<br>
+<input class="input" id="username" name="username"size="20"
+type="text" value=""></label></p>
+<p><label for="user_pass">Пароль<br>
+ <input class="input" id="password" name="password"size="20"
+  type="password" value=""></label></p> 
+	<p class="submit"><input class="button" name="login"type= "submit" value="Log In"></p>
+	<p class="regtext">Еще не зарегистрированы?<a href= "register.php">Регистрация</a>!</p>
+   </form>
+ </div>
+  </div>
+<?php include("includes/footer.php"); ?>
