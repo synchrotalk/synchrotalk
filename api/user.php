5513901019621ebdae302e923e4fd71224320080
Mikhail Kasyanov <michaelrain@exsul.net>
2015-09-15 11:45:56 -0400

    Phoxy Basics

---
+++ api/user.php
@@ -0,0 +1,12 @@
+<?php
+class user extends api{
+  protected function Reserve(){
+    return "User reserve";
+  }
+  protected function music(){
+    return "Hello";
+  }
+  protected function main(){
+    return "world";
+  } 
+}
---
+++ api/user/music.php
@@ -0,0 +1,11 @@
+<?php
+
+class music extends api {
+  protected function Reserve(){
+    return "Music reserve";
+  }
+  protected function play(){
+
+  }
+  
+}
