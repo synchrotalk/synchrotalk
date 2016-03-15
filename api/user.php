48b4f65f13ca9229504b4aea66ff0b9093783a41
Orest Oliinyk <orest.oliinyk@exsul.net>
2016-03-15 02:33:33 +0100

    Login system v0.01

--- api/user.php
+++ api/user.php
@@ -16,7 +16,20 @@ class user extends api
     return
     [
       'design' => 'user/login',
+      'data' => [
+        'username' => $this->GetUserName(),
+      ],
     ];
+  }
+
+  protected function GetUserName()
+  {
+    session_start();
+    if(isset($_SESSION['username'])){
+      return $_SESSION['username'];
+    }
+
+    return false;
   }
 
 }
