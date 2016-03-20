3a68e6745e2edff3e7c0866da35e53d26554ccad
Orest Oliinyk <orest.oliinyk@exsul.net>
2016-03-20 18:29:22 +0100

    you have no auth implementation yet. Rename to login. Learn what auth mean.

--- api/user.php
+++ api/user.php
@@ -11,23 +11,12 @@ class user extends api
     ];
   }
 
-  protected function login()
+  protected function login($username="")
   {
-    return
-    [
-      'design' => 'user/login',
-      'data' => [
-        'username' => $this->GetUserName(),
-      ],
-    ];
-  }
-
-  protected function auth($username)
-  {
+    session_start();
     $login = false;
     if($username!=="")
     {
-      session_start();
       $_SESSION['username'] = $username;
       $login = true;
     }
@@ -35,13 +24,14 @@ class user extends api
     [
       'design' => 'user/login',
       'login' => $login,
+      'data' => [
+        'username' => $this->GetUserName(),
+      ],
     ];
   }
 
-
   protected function GetUserName()
   {
-    session_start();
     if(isset($_SESSION['username'])){
       return $_SESSION['username'];
     }
