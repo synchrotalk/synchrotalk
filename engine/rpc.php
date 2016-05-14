34d04498bf5bbb283067471b4bbc326ac3652439
Kirill Berezin <enelar@exsul.net>
2016-05-14 04:20:12 +0300

    Since we yet do not use database, stop connecting it

--- engine/rpc.php
+++ engine/rpc.php
@@ -35,7 +35,7 @@ phoxy_return_worker::$add_hook_cb = function($that)
 };
 
 error_reporting(E_ALL & ~E_STRICT & ~E_NOTICE);
-phpsql\OneLineConfig(conf()->db->connection_string);
+//phpsql\OneLineConfig(conf()->db->connection_string);
 
 try
 {
