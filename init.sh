adb926bde81caa3669cbd66664072899189b3df9
Kirill Berezin <enelar@exsul.net>
2016-05-16 07:28:46 +0300

    Init snippets link too

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 # Initialise repository for development or deploy on vps
 composer update
 ln -s vendor/phoxy/phoxy/ phoxy
+ln -s ../vendor/phoxy/snippets ejs/snippets
 touch secret.yaml
 mkdir networks
 
