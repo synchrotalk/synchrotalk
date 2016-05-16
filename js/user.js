32f9df9010cec1e0d46c6860b5bf5a5554917a20
Kirill Berezin <enelar@exsul.net>
2016-05-16 06:09:33 +0300

    Use current user data taked from login

--- js/user.js
+++ js/user.js
@@ -10,4 +10,23 @@ user.login = function(cb, r)
   cb();
 }
 
+user.author = function(id, users)
+{
+  if (id == window.user.id)
+    return window.user;
+
+  for (var k in users)
+  {
+    var v = users[k];
+
+    if (v.id == id)
+      break;
+  }
+
+  if (v.id != id)
+    notify("error", "Unable to find author " + id);
+
+  return v;
+}
+
 window.user = user;
