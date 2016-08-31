479fdaea908abd6da264c4c7feca1d56b07e9ba0
Kirill Berezin <enelar@exsul.net>
2016-08-31 21:32:32 +0300

    Drafting ajax wait until cache

---
+++ js/ajax-booster.js
@@ -0,0 +1,55 @@
+var ajax_booster =
+{
+  init()
+  {
+    phoxy.state.ajax.booster = {};
+  }
+  ,
+  request: function(url, cb, data)
+  {
+    this.schedule(url, cb, data);
+  }
+  ,
+  schedule: function(url, cb, data)
+  {
+    var task =
+    {
+      cb: cb,
+      data: data,
+    };
+
+    if (typeof phoxy.state.ajax.booster[url] != 'undefined')
+      return phoxy.state.ajax.booster[url].push(task);
+
+    phoxy.state.ajax.booster[url] = [task];
+    this.execute_next(url);
+  }
+  ,
+  execute_next: function(url)
+  {
+    var task = phoxy.state.ajax.booster[url].shift();
+
+    var shedule_next = function()
+    {
+      ajax_booster.finished(url);
+      task.cb.apply(phoxy, arguments);
+    };
+
+    this.ajax(url, shedule_next, task.data);
+  }
+  ,
+  finished: function(url)
+  {
+    if (phoxy.state.ajax.booster[url].length > 0)
+      return this.execute_next(url);
+
+    delete phoxy.state.ajax.booster[url];
+  }
+  ,
+  ajax: function(url, cb, data)
+  {
+    phoxy._.EarlyStage.ajax(url, cb)
+  }
+};
+
+ajax_booster.init();
